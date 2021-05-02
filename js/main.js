726ebea3ccac9c5d93344a8a568886c6037d4aeb
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-05-02 17:00:10 +0000

    Daek theme with localStorage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+    if(localStorage.getItem('darkEnabled') == "true"){
+        $("html").addClass('darkMode');
+    }else{
+        $("html").removeClass('darkMode');
+    }
     // delete user
     deleteUser();
     // add user
@@ -8,5 +13,11 @@ $(document).ready(function(){
 
     $("#dark-mode").click(function () {
         $("html").toggleClass('darkMode');
+        if(localStorage.getItem('darkEnabled') == "true"){
+            localStorage.setItem('darkEnabled', 'false');
+        }else{
+            localStorage.setItem('darkEnabled', 'true');
+        }
+        console.log(localStorage.getItem('darkEnabled'));
     });
 });
